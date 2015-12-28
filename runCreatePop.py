e4881f4a598256e3d04241aeabe30ef772cd17ee
jsemple19 <semple.j@gmail.com>
2015-12-28 18:01:18 +0100

    Update runCreatePop.py

--- runCreatePop.py
+++ runCreatePop.py
@@ -9,16 +9,6 @@ random.seed(mySeed)
 
 loci=15000
 mutRate=3.1e-09
-#chrRegions=np.array([0, 0.5, 4.5, 12.5, 16.5, 17]*loci/17,dtype=float)
-#regRecRates=np.array([0, 7, 1, 7, 0]/15, dtype=float)
-
-#qtls1=np.array([0.2*loci],dtype=int)
-#qtls2=np.array([0.5*loci],dtype=int)
-#qtls3=np.array([0.15*loci, 0.5*loci],dtype=int)
-#qtls4=np.array([0.05*loci, 0.2*loci],dtype=int)
-#qtls5=np.array([0.35*loci, 0.5*loci],dtype=int)
-#qtls6=np.array([0.2*loci, 0.35*loci],dtype=int)
-
 
 #create vector of recombination rate along chr specific for C. elegans
 recRate=celegRR(loci)
@@ -36,10 +26,6 @@ founder2.createFounder(genotype=1)
 numSims=10
 Ns=[5000]#[100,500,1000,5000,10000,50000,100000]
 Gs=[5,10,15,20,25,30]
-#selectDepths=[1,5,10,20,30,40,50]
-#seqDepths=[25,50,100,150,200,250]
-#qtls=[qtls1,qtls2,qtls3]
-#effectSize=[1]
 useRLE=True
 
 
@@ -62,14 +48,3 @@ for N in Ns:
             population.createRIpop(G)
             writePopToFile(population,baseDir,numSim)
 
-#fig=plt.plot(range(loci),alleleFreq1,'r',range(loci),alleleFreq,'b')
-#plt.axis([0,loci,0,1])
-#plt.xlabel('loci')
-#plt.ylabel('allele frequency')
-#plt.suptitle('whole genome vs per-locus selection for seq',fontsize=13)
-#plt.vlines(qtls3,0,1,colors='0.4',linestyles='dashed')
-#plt.hlines(0.5,0,loci,colors='0.4',linestyles='dotted')
-#plt.title('N=1000, gen=5, selectDepth=0.1, seqDepth=100',{'fontsize':9})
-#plt.figlegend(fig,('per-locus','whole-genome'),loc='lower right', 
-#              bbox_to_anchor=(0.85, 0.2),fontsize=10)
-#plt.savefig('genomeVslocus.pdf',papertype='a4',format='pdf')
